feee83dc8122332582eb3e90dfe9846096b38523
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-06 17:54:34 -0400

    changed LoginService to handle a null username/password field

--- src/main/java/com/revature/services/LoginService.java
+++ src/main/java/com/revature/services/LoginService.java
@@ -12,12 +12,11 @@ public class LoginService {
 
     public Optional<AppUser> authenticate(String username, String password) {
 
-        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
-            throw new InvalidRequestException("Invalid credentials, try again.");
-        }
+//        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
+//            throw new InvalidRequestException("Invalid credentials, try again.");
+//        }
 
         Optional<AppUser> authorizedUser = logDAO.findUserByLogin(username, password);
-                                            //.orElseThrow(AuthenticationException::new);
 
         return authorizedUser;
     }
