e577cbe790d3f1abad92808282afa91174454890
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-07 16:34:24 -0400

    changed welcome file to go to login page for application

--- src/main/java/com/revature/servlets/MasterServlet.java
+++ src/main/java/com/revature/servlets/MasterServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/api/*")
+
 public class MasterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
